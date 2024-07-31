c017b732698dca1b58005704460196ba4473a7ee
sanika <sanika.peshkar@coditas.com>
2024-07-31 17:40:35 +0530

    fixed images

--- src/DessertData.js
+++ src/DessertData.js
@@ -1,67 +1,76 @@
-import Waffle from '/assets/images/image-waffle-desktop.jpg'
+
+import Crispy from './assets/images/image-waffle-desktop.jpg';
+import Creme from './assets/images/image-creme-brulee-desktop.jpg';
+import Macron from './assets/images/image-macaron-desktop.jpg';
+import Tiramisu from './assets/images/image-tiramisu-desktop.jpg';
+import Baklava from './assets/images/image-baklava-desktop.jpg';
+import Pie from './assets/images/image-pie-desktop.jpg';
+import Cake from './assets/images/image-cake-desktop.jpg';
+import Brownie from './assets/images/image-brownie-desktop.jpg';
+import PannaCotta from './assets/images/image-panna-cotta-desktop.jpg';
 
 export const DESSERTS = [
   {
     _id: "1",
     dessertName: "waffle",
     dessertPrice: 4.99,
-    imgUrl:'assets/images/image-waffle-desktop.jpg',
+    imgUrl: Crispy,
     description: "Crispy and fluffy",
   },
   {
     _id: "2",
     dessertName: "creme brulee",
     dessertPrice: 6.99,
-    imgUrl: 'assets/images/image-creme-brulee-desktop.jpg',
+    imgUrl: Creme,
     description: "Rich and creamy",
   },
   {
     _id: "3",
     dessertName: "macaron",
     dessertPrice: 3.99,
-    imgUrl: 'assets/images/image-macaron-desktop.jpg',
+    imgUrl: Macron,
     description: "Delicate and colorful",
   },
   {
     _id: "4",
     dessertName: "tiramisu",
     dessertPrice: 5.99,
-    imgUrl: 'assets/images/image-tiramisu-desktop.jpg',
+    imgUrl: Tiramisu,
     description: "Coffee-flavored delight",
   },
   {
     _id: "5",
     dessertName: "baklava",
     dessertPrice: 4.49,
-    imgUrl:'assets/images/image-waffle-desktop.jpg',
+    imgUrl: Baklava,
     description: "Sweet and nutty",
   },
   {
     _id: "6",
     dessertName: "pie",
     dessertPrice: 3.49,
-    imgUrl: '/assets/images/image-waffle-desktop.jpg',
+    imgUrl: Pie,
     description: "Fruit-filled pastry",
   },
   {
     _id: "7",
     dessertName: "cake",
     dessertPrice: 5.49,
-    imgUrl: '/assets/images/image-cake-desktop.jpg',
+    imgUrl: Cake,
     description: "Soft and moist",
   },
   {
     _id: "8",
     dessertName: "brownie",
     dessertPrice: 2.99,
-    imgUrl: '/assets/images/image-brownie-desktop.jpg',
+    imgUrl: Brownie,
     description: "Fudgy and chocolaty",
   },
   {
     _id: "9",
     dessertName: "panna cotta",
     dessertPrice: 4.99,
-    imgUrl: '/assets/images/image-waffle-desktop.jpg',
+    imgUrl: PannaCotta,
     description: "Creamy and silky",
   },
 ];
